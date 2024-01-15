97968517de832d720e06b15c96f2389488af905c
unknown <victoriasugden@yahoo.co.uk>
2024-01-15 14:37:26 +0000

    started changing questions to objects

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -18,10 +18,10 @@ answer: 'B) Building Interactive Web Pages'
    'A) let',
     'B) const',
     'C) var',
-    'D) const and let'
+    'D) const and let',
 ],
     answer: 'B) const'
-}
+},
 
 {
 
@@ -32,50 +32,87 @@ choices:
 'B) Print messages to the console',
 'C) Create a new webpage',
 'D) Define a new function',
-]
+],
+    answer: 'B) Print messages to the console'
+},
+
+{
+questions '4. Which keyword is used to declare a variable with a block scope?'
+[
+'A) var',
+'B) let',
+'C) const'
+'D) Both let and const',
+],
+
+answer: ''
+
+},
+
+{
+question: '5. How do you write a single-line comment in JavaScript?',
+
+[
+'A) /* Comment */',
+'B) # Comment',
+'C) // Comment',
+'D) <!-- Comment -->',
+
+],
+answer: 'C) // Comment'
+
+
+},
+
+}
+question: '6. What is the correct way to create an array in JavaScript?',
+
+[
+'A) array = [1, 2, 3]'
+'B) let array = {1, 2, 3}'
+'C) let array = [1, 2, 3]'
+'D) array(1, 2, 3)'
+],
+answer: 'array = [1, 2, 3]'
+},
+
+
+}
+questions: '7. How do you check if two variables are strictly equal in value and type?',
+
+[
+'A) ==',
+'B) ===',
+'C) =',
+'D) !==,'
+],
+
+answer:'B) ==='
+},
+
+
+{
+question: '8. What does the for loop in JavaScript do?',
+
+[
+'A) Executes a block of code repeatedly',
+'B) Declares a variable',
+'C) Adds two numbers',
+'D) Defines a function',
+],
+answer: 'A) Executes a block of code repeatedly'
+},
+
+{
+question: '9. What is the purpose of a JavaScript function?',
+[
+'A) Print messages to the console',
+'B) Create a new webpage',
+'C) Reuse a block of code for a specific task',
+'D) Connect to a database',
 }
 
-4. Which keyword is used to declare a variable with a block scope?
-
-A) var
-B) let
-C) const
-D) Both let and const
-5. How do you write a single-line comment in JavaScript?
-
-A) /* Comment */
-B) # Comment
-C) // Comment
-D) <!-- Comment -->
-
-6. What is the correct way to create an array in JavaScript?
-
-A) array = [1, 2, 3]
-B) let array = {1, 2, 3}
-C) let array = [1, 2, 3]
-D) array(1, 2, 3)
-
-7. How do you check if two variables are strictly equal in value and type?
-
-A) ==
-B) ===
-C) =
-D) !==
-
-8. What does the for loop in JavaScript do?
-
-A) Executes a block of code repeatedly
-B) Declares a variable
-C) Adds two numbers
-D) Defines a function
-
-9. What is the purpose of a JavaScript function?
-
-A) Print messages to the console
-B) Create a new webpage
-C) Reuse a block of code for a specific task
-D) Connect to a database
-
+}
 10. Which of the following is used to print a message to the console?
 - A) print()
 - B) log()
